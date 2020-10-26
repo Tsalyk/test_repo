dbb21920f2e374f6e10d1143e6641c38870f0323
Маркіян Цалик <markiian_tsalyk@Markians-MacBook-Pro.local>
2020-10-26 21:59:02 +0200

    lesson 2

---
+++ Desktop/ITEA/Lesson2/project.py
@@ -0,0 +1,13 @@
+a = int(input())
+
+if a == 0:
+    print(a, "is zero")
+elif a < 0:
+    print(a, "is negative")
+elif a > 0:
+    print(a, "is positive")
+
+if a % 2 == 0:
+    print(a, "is evel")
+else:
+    print(a, "is non-odd")
